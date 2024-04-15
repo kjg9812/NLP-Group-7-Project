dcd738eefa805541a6e9eecab6809546d5e2f14a
Kevin Garcia <kjg9812@nyu.edu>
2024-04-14 23:54:46 -0400

    instructions for split code

--- data_process.py
+++ data_process.py
@@ -30,6 +30,9 @@ def main():
     train_df.reset_index(drop=True, inplace=True)
     test_df.reset_index(drop=True, inplace=True)
 
+    print(train_df.shape[0])
+    # 13 percent of total are the query songs
+
     # we need to separate queries and abstracts
     # take 300 (arbitrary number, is there a better number?) songs in training to be query songs
     # Take the first 300 rows from the original DataFrame
@@ -42,21 +45,36 @@ def main():
     query_songs.reset_index(drop=True, inplace=True)
     train_df.reset_index(drop=True, inplace=True)
 
-
     # the rest of the songs, combine similar genre songs to make a complete abstract
         # an abstract can consist of 50 songs with the same genre (this is an parbitrary number? is there a number that it should acc be?)
+        # is there any detriment to using all the songs in the genre to represent one abstract?
     # can also experiment with just song similarity by just making a single song an abstract
+
+    # debug some counts for each genre
+    print(train_df['Genre'].value_counts())
 
     # then we'll have abstracts (consisting of combined lyrics) that are labeled with a genre
     # and query songs
         # make an answer key for the query songs
             # a file that on every line consists of "song name, genre"
+
+    
+
+
+    # FOR KEVIN H
+    # assume you have a list of lists (each sub list is a vector) for both queries and abstracts
     # compare query songs to every abstract and get cosine similarity scores, sort by highest
         # make an output file that consists of "song name, genre, score"
         # genre maybe comes from a dictionary that maps an abstract to its genre
     # go through this output file and produce a final output file that is similar to answer key in format "song name, genre"
         # pick the highest scores in the previous file
     # score the model by comparing to the answer key
+
+    # format of answer key and final output file
+    # each line:
+    # song1 pop
+    # song2 rock
+    #....
 
 
 if __name__ == "__main__":
