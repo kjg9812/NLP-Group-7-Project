cbe6a00dae1bddca1a1459b2acad618c82ca35d7
Kevin Garcia <kjg9812@nyu.edu>
2024-05-14 00:37:59 -0400

    deletes

--- datasetwrapper.py
+++
@@ -1,6 +0,0 @@
-# import hdf5_getters
-
-
-# h5 = hdf5_getters.open_h5_file_read(path to some file)
-# duration = hdf5_getters.get_duration(h5)
-# h5.close()
--- main.py
+++
@@ -1,8 +0,0 @@
-
-# main should call functions using the training, development, and test sets
-# it should also evaluate the model and output accuracy
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
